4e06c3a41c7041ff786b666dc75cd41873d5b3ed
Carlos Daniel Valente Garcia <60764608+Daniel-Valente@users.noreply.github.com>
2023-05-17 17:20:35 -0600

    Added new features
    Environment configuration

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { connection } = require('./services/connection');
+
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }));
+
+app.get( '/', ( req, res ) => res.status(200).json({ message: "This server is online", connection: connection.threadId }) );
+
+app.use( '/task', require('./routers/task'));
+
+app.listen( port, () => console.log(` This server is running on port ${ port }`));
