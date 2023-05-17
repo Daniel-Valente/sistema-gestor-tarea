9b483fa5664b7539998299fbf7f0ec3885925009
Carlos Daniel Valente Garcia <60764608+Daniel-Valente@users.noreply.github.com>
2023-05-17 17:51:58 -0600

    Added new features
    Added middleware about tasks

---
+++ routers/task.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const { connection } = require('../services/connection');
+
+const router = Router();
+
+router.get( '/', async( req, res ) =>{});
+
+router.get( '/:idtarea', async( req, res ) =>{});
+
+router.get( '/search', async( req, res ) =>{});
+
+router.post( '/', async( req, res ) =>{});
+
+router.put( '/:idtarea', async( req, res ) =>{});
+
+router.delete( '/:idtarea', async( req, res ) =>{});
