5536920efc01076833fe71986c713ed04ed600d4
Carlos Daniel Valente Garcia <60764608+Daniel-Valente@users.noreply.github.com>
2023-05-17 19:21:23 -0600

    Added new feature
    Added Get task by id

--- routers/task.js
+++ routers/task.js
@@ -30,7 +30,22 @@ router.get( '/', async( req, res ) =>{
     }
 });
 
-router.get( '/:idtarea', async( req, res ) =>{});
+router.get( '/:idtarea', async( req, res ) =>{
+    try {
+        const { idtarea } = req.params;
+        if( +idtarea < 0 ) return res.status( 400 ).send(['This param is not valid']);
+
+        connection.query(`SELECT * FROM tarea WHERE idtarea = ${ +idtarea }`, ( err, result, fields ) => {
+            if( err ) return res.status( 400 ).send([ err.message ]);
+            if( !result.length ) return res.status( 400 ).send(['Content not found by this idtarea']);
+            
+            return res.status(200).json( result );
+        });
+
+    } catch (error) {
+        return res.status( 500 ).send([ error.message ]);
+    }
+});
 
 router.get( '/search', async( req, res ) =>{});
 
